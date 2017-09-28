fb29e1238e2958e3de3e06d5077bda34ab8561f6
carlosv123 <vasquezcarlos1996@gmail.com>
2017-09-28 08:00:06 -0700

    initial commit

---
+++ BinarySearchTreeProject/BinarySearchTree/Node.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+
+
+
+    public class Node
+    {
+        public int data;
+        public Node leftchild;
+        public Node rightchild; 
+
+        public Node(int data)
+        {
+            this.data = data;
+            leftchild = null;
+            rightchild = null; 
+        }
+        
+
+    }
+  
+}
---
+++ BinarySearchTreeProject/BinarySearchTree/binarysearchtree.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+   public class binarysearchtree
+    {
+
+        public Node root;
+
+        public binarysearchtree()
+        {
+            root = null;
+        }
+        public int addnumber(int data)
+        {
+            Node newnode = new Node(int data);
+            newnode.data = i;
+            if (root == null)
+                root = newnode;
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                }
+             }
+            if (current == null)
+            {
+                parent.rightchild = newndode;
+            }
+            else
+            {
+               if(current == null)
+                {
+                    parent.leftchild = new Node; 
+                }
+            }
+           public search(Node node,int data)
+        {
+            if (node.data != data)
+            {
+                return null;
+            }
+            else
+            {
+                if (node == null)
+                {
+                    return null;
+                }
+                else if (data < node.data)
+                {
+                    return search(leftchild);
+                }
+                else (data > node.data)
+                {
+                    return search(rightchild);
+                }
+            }
+        } 
+
+            
+
+        }
+    }
+}  
